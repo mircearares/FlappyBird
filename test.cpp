3de8f70dc0a095eabda1cb69e0015cf713d55252
mircearares <mirceararesgabriel@gmail.com>
2017-01-11 11:01:45 +0200

    Updates 2

--- test.cpp
+++ test.cpp
@@ -5,49 +5,65 @@
 #include<windows.h>
 #include<fstream>
 #include<algorithm>
+#include<graphics.h>
+#include<time.h>
 using namespace std;
 
 fstream file("rez.txt", ios::in | ios::out | ios::app);
 
-int results[100], nrRes=0;
-
-void addResult(int rez)
-{
-    results[++nrRes]=rez;
-}
-
-void readResults()
-{
-    int x;
-    while(file>>x)
-        addResult(x);
-}
-
-void writeResults(int rez)
-{
-    file.clear();
-    file<<rez<<"\n";
-    file.close();
-}
-
-
-void writeResultsConsole()
-{
-    readResults();
-    sort(results, results+nrRes);
-    for(int i=nrRes-1;i>=0;i--)
-        cout<<results[i]<<"\n";
-}
-
 char a[23][98];
-int canColide=1;
+int canColide=1, nrRes=0;
 
 typedef struct
 {
     int x, y;
 }PASARE;
 
-int collision(PASARE bird)
+PASARE bird;
+
+typedef struct
+{
+    int score, time;
+}RESULTS;
+
+RESULTS result[100];
+
+void addResult(int rez, int tim)
+{
+    result[nrRes].score=rez;
+    result[nrRes].time=tim;
+    nrRes++;
+}
+
+void readResults()
+{
+    int rez, tim;
+    while(file>>rez>>tim)
+        addResult(rez, tim);
+}
+
+void writeResults(int rez, int tim)
+{
+    file.clear();
+    file<<rez<<" "<<tim<<"\n";
+    file.close();
+}
+
+bool comp(RESULTS a, RESULTS b)
+{
+    if(a.score<b.score)
+        return 1;
+    return 0;
+}
+
+void writeResultsConsole()
+{
+    readResults();
+    sort(result, result+nrRes, comp);
+    for(int i=nrRes-1;i>=0;i--)
+        cout<<result[i].score<<" "<<result[i].time<<"s\n";
+}
+int collision()
 {
    if(a[bird.y-1][bird.x]=='#' && a[bird.y-1][bird.x-1]=='#' && a[bird.y-1][bird.x+1]=='#')
         return 0;
@@ -59,7 +75,7 @@ int collision(PASARE bird)
     return 0;
 }
 
-int collisionPowerUp(PASARE bird)
+int collisionPowerUp()
 {
    if((a[bird.y][bird.x]=='B' && a[bird.y][bird.x+1]=='W')
       ||(a[bird.y][bird.x]=='B' && a[bird.y][bird.x+1]=='R')
@@ -69,7 +85,8 @@ int collisionPowerUp(PASARE bird)
    return 0;
 
 }
-void drawBird(PASARE bird)
+
+void drawBird()
 {
     a[bird.y][bird.x]='B';
     a[bird.y][bird.x-1]=0;
@@ -81,7 +98,7 @@ void drawLand()
         a[21][i]='#';
 }
 
-int collisionLand(PASARE bird)
+int collisionLand()
 {
     if(bird.y==21)
         return 1;
@@ -89,7 +106,7 @@ int collisionLand(PASARE bird)
 }
 
 // astea 2 pot sa le unesc intr-un singur subprogram mai incolo
-void movUpBird(PASARE &bird)
+void movUpBird()
 {
     a[bird.y][bird.x]=0;
     bird.y--;
@@ -97,7 +114,7 @@ void movUpBird(PASARE &bird)
     Beep(200,50);
 }
 
-void movDnBird(PASARE &bird)
+void movDnBird()
 {
     a[bird.y][bird.x]=0;
     bird.y++;
@@ -118,7 +135,7 @@ void genObst()
 
 }
 
-void movObst(PASARE bird)
+void movObst()
 {
     for(int i=0;i<22;i++)
         for(int j=0;j<97;j++)
@@ -148,14 +165,14 @@ void genPowerUp()
     a[poz+1][95]='R';
 }
 
-bool addScore(PASARE bird)
+bool addScore()
 {
-    if(a[0][bird.x]=='#' && a[20][bird.x] && !collision(bird))
+    if(a[0][bird.x]=='#' && a[20][bird.x] && !collision())
         return true;
     return false;
 }
 
-void deletePowerUp(PASARE bird)
+void deletePowerUp()
 {
     if((a[bird.y][bird.x]=='B' && a[bird.y][bird.x+1]=='W')
       ||(a[bird.y][bird.x]=='B' && a[bird.y][bird.x+1]=='R')
@@ -212,10 +229,12 @@ void powerUp()//trebuie testate fiecare in parte
 void game()
 {
     int nr=0, score=0, powerUpHit=0, timer=0;
-    PASARE bird;
+    float time_elapsed=0;
+    const clock_t begin_time = clock();
+
     bird.x=5;
     bird.y=10;
-    drawBird(bird);
+    drawBird();
     drawLand();
 
     while(1)
@@ -230,27 +249,28 @@ void game()
             else if((nr+1)%105==0 && !powerUpHit)
                 genPowerUp();
 
-            drawBird(bird);
+            drawBird();
 
             if(GetAsyncKeyState(VK_SPACE))
-                movUpBird(bird);
+                movUpBird();
             else
-                movDnBird(bird);
+                movDnBird();
 
-            if((collision(bird) || collisionLand(bird)) && canColide)
+            if((collision() || collisionLand()) && canColide)
             {
                 system("cls");
                 cout<<"Scorul dvs. este: "<<score;
-                writeResults(score);
+                time_elapsed=(clock()-begin_time)/CLOCKS_PER_SEC;
+                writeResults(score, time_elapsed);
                 Sleep(1500);
                 return;
             }
-            if(collisionPowerUp(bird))
+            if(collisionPowerUp())
             {
                 powerUp();
                 powerUpHit=1;
                 timer=0;
-                deletePowerUp(bird);
+                deletePowerUp();
             }
 
             if(powerUpHit)
@@ -258,20 +278,19 @@ void game()
             if(powerUpHit && timer==20)
                 resetValues();
 
-            if(addScore(bird))
+            if(addScore())
             {
                 Beep(400,50);
                 score++;
             }
 
             nr++;
-            movObst(bird);
+            movObst();
             afis();
             Sleep(sleep_value);
         }
     }
 }
-
 /*
 int main()
 {
@@ -279,4 +298,5 @@ int main()
     return 0;
 }
 */
+
 
