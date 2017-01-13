38ce0f3754bb5cba8ce21311487d5c0089a98105
mircearares <mirceararesgabriel@gmail.com>
2017-01-13 23:28:25 +0200

    98% done

---
+++ Game/results.cpp
@@ -0,0 +1,53 @@
+#include"biblioteci.cpp"
+
+fstream file("rez.txt", ios::in | ios::out | ios::app);
+
+int nrRes=0;
+
+typedef struct
+{
+    int score, time;
+}RESULTS;
+
+RESULTS result[100];
+
+void addResult(int rez, int tim)
+{
+    result[nrRes].score=rez;
+    result[nrRes].time=tim;
+    nrRes++;
+}
+
+void readResults()
+{
+    int rez, tim;
+    while(file>>rez>>tim)
+        addResult(rez, tim);
+}
+
+void writeResults(int rez, int tim)
+{
+    file.clear();
+    file<<rez<<" "<<tim<<"\n";
+    file.close();
+}
+
+bool comp(RESULTS a, RESULTS b)
+{
+    if(a.score<b.score)
+        return 1;
+    else if(a.time<b.time)
+        return 1;
+    return 0;
+}
+
+void writeResultsConsole()
+{
+    readResults();
+    sort(result, result+nrRes, comp);
+    cout<<"Place    Score     Time \n";
+    for(int i=nrRes-1;i>=0;i--)
+       cout<<"  "<<nrRes-i<<".      "<<result[i].score<<"      "<<result[i].time<<"s\n";
+
+
+}
---
+++ Game/test.cpp
@@ -0,0 +1,91 @@
+#include"biblioteci.cpp"
+#include"bird.cpp"
+#include"results.cpp"
+#include"powerups.cpp"
+#include"map.cpp"
+
+void loseGame(int score, int time)
+{
+    system("cls");
+    cout<<"         You lost          \n";
+    cout<<"    Score         Time \n";
+    cout<<"     "<<score<<"             "<<time<<"s";
+    Sleep(1500);
+}
+
+bool addScore()
+{
+    if(a[0][bird.x]=='#' && a[20][bird.x] && !collision())
+        return true;
+    return false;
+}
+
+void game()
+{
+    int nr=0, score=0, powerUpHit=0, timer;
+    float time_elapsed=0;
+    const clock_t begin_time = clock();
+
+    bird.x=5;
+    bird.y=10;
+    drawBird();
+    drawLand();
+
+    while(1)
+    {
+        system("cls");
+        while(1)
+        {
+            if(GetAsyncKeyState(VK_RETURN))
+                return;
+            if(nr%15==0)
+                genObst();
+            else if((nr+1)%55==0 && !powerUpHit)
+                genPowerUp();
+
+            drawBird();
+
+            if(GetAsyncKeyState(VK_SPACE))
+                movUpBird();
+            else
+                movDnBird();
+
+            if((collision() || collisionLand()) && canColide)
+            {
+                system("cls");
+                resetMap();
+
+                time_elapsed=(clock()-begin_time)/CLOCKS_PER_SEC;
+                writeResults(score, time_elapsed);
+
+                loseGame(score, time_elapsed);
+                Sleep(1500);
+                return;
+            }
+            if(collisionPowerUp())
+            {
+                powerUp();
+                powerUpHit=1;
+                timer = clock()/CLOCKS_PER_SEC;
+            }
+
+            if(powerUpHit &&(clock()/CLOCKS_PER_SEC)-timer== 10)
+            {
+                resetValues();
+                powerUpHit=0;
+            }
+
+            if(addScore())
+            {
+                Beep(400,50);
+                score++;
+            }
+
+            nr++;
+            movObst();
+            movPowerUp();
+            afis();
+            Sleep(sleep_value);
+        }
+    }
+}
